bd7b45cec83aba92c3312d05509fd0de7da4be5c
vivekb050290 <vivekb050290@gmail.com>
2022-11-13 00:06:20 +0530

    PromotionsController added

---
+++ PromotionEngine/PromotionEngineWebApp/Controllers/PromotionsController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PromotionEngine.Store;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PromotionEngineWebApp.Controllers
+{
+    [Route("api/Promotions")]
+    [ApiController]
+    public class PromotionsController : ControllerBase
+    {
+        private readonly IStore Store;
+
+        public PromotionsController(IStore store)
+        {
+            Store = store;
+        }
+
+        // GET: api/<PromotionsController>
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> GetAllPromotions()
+        {
+            try
+            {
+                var promotions = Store.GetPromotions();
+                return promotions == null || promotions.Count() == 0 ? 
+                    Ok(new List<string>()) :
+                    Ok(promotions.Select(p => p.ToString()));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        // POST api/<PromotionsController>
+        [HttpPost("{promotion}")]
+        public ActionResult<string> PostPromotion(string promotion)
+        {
+            try
+            {
+                Store.AddPromotion(promotion);
+
+                return CreatedAtAction(
+                    nameof(GetAllPromotions),
+                    new { id = promotion },
+                    promotion);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        // DELETE api/<PromotionsController>/5
+        [HttpDelete("{promotion}")]
+        public ActionResult<string> Delete(string promotion)
+        {
+            try
+            {
+                Store.DeletePromotion(promotion);
+                return Ok(promotion);
+
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
