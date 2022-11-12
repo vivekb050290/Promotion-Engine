ffa629ed12d2678dd8eba8b67adc832fdb485b8a
vivekb050290 <vivekb050290@gmail.com>
2022-11-12 23:51:48 +0530

    get sku by id fix for invalid sku

--- PromotionEngine/PromotionEngineWebApp/Controllers/SKUitemController.cs
+++ PromotionEngine/PromotionEngineWebApp/Controllers/SKUitemController.cs
@@ -5,11 +5,8 @@ using PromotionEngineWebApp.DTO;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using PromotionEngineWebApp.DTO;
-
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace PromotionEngineWebApp.Controllers
 {
     [Route("api/SKUitems")]
@@ -21,13 +18,10 @@ namespace PromotionEngineWebApp.Controllers
         {
             Store = store;
         }
-
         // GET: api/<SKUitemController>
         [HttpGet]
-        public ActionResult<IEnumerable<SKUitem>> GetAllSKUitems()
         public ActionResult<IEnumerable<SKUitemDTO>> GetAllSKUitems()
         {
-            return Store.GetSKUitems();
             try
             {
                 return Ok(Store.GetSKUitems().Select(s => s.ToDTO()));
@@ -37,35 +31,31 @@ namespace PromotionEngineWebApp.Controllers
                 return BadRequest(e.Message);
             }
         }
-
         // GET api/<SKUitemController>/5
         [HttpGet("{id}")]
-        public ActionResult<SKUitem> GetSKUitem(string id)
         public ActionResult<SKUitemDTO> GetSKUitem(string id)
         {
-            return Store.GetSKUitems().FirstOrDefault(i => id.Equals(i.ID));
             try
             {
                 return Ok(Store.GetSKUitems().FirstOrDefault(i => id.Equals(i.ID)).ToDTO());
+                var skuItem = Store.GetSKUitems().FirstOrDefault(i => id.Equals(i.ID));
+                return skuItem != null ?
+                    Ok(skuItem.ToDTO()) :
+                    BadRequest("SKU not found!");
             }
             catch (Exception e)
             {
                 return BadRequest(e.Message);
             }
         }
-
         // POST api/<SKUitemController>
         [HttpPost]
-        public void PostSKUitem(SKUitemDTO item)
         public ActionResult<SKUitemDTO> PostSKUitem(SKUitemDTO item)
         {
-            Store.AddSKUitem(new SKUitem(item.ID, item.UnitPrice));
             try
             {
                 var itemDao = item.ToDAO();
-
                 Store.AddSKUitem(itemDao);
-
                 return CreatedAtAction(
                     nameof(GetSKUitem),
                     new { id = item.ID },
@@ -76,35 +66,27 @@ namespace PromotionEngineWebApp.Controllers
                 return BadRequest(e.Message);
             }
         }
-
         // PUT api/<SKUitemController>/5
         [HttpPut("{skuID}")]
-        public void Put(string skuID, float unitPrice)
         public ActionResult<SKUitemDTO> Put(string skuID, float unitPrice)
         {
-            Store.UpdateSKUitemUnitPrice(skuID, unitPrice);
             try
             {
                 Store.UpdateSKUitemUnitPrice(skuID, unitPrice);
-
                 return CreatedAtAction(
                     nameof(GetSKUitem),
                     new { id = skuID },
                     new SKUitem(skuID, unitPrice).ToDTO());
-
             }
             catch (Exception e)
             {
                 return BadRequest(e.Message);
             }
         }
-
         // DELETE api/<SKUitemController>/5
         [HttpDelete("{skuID}")]
-        public void DeleteSKU(string skuID)
         public ActionResult<string> DeleteSKU(string skuID)
         {
-            Store.DeleteSKUitem(skuID);
             try
             {
                 Store.DeleteSKUitem(skuID);
